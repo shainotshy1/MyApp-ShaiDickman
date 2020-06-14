620dee40d15d2b24fb29d77beeafcf4b3971fd1c
shainotshy1 <shai.j.dickman@gmail.com>
2020-06-14 10:52:08 -0700

    Commit.6.14

---
+++ MyApp/MyApp/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+
+namespace MyAppMainPage
+
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            EraseCommand = new Command(() => { TheNote = string.Empty; });
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+
+                TheNote = string.Empty;
+            });
+        }
+        public ObservableCollection<string> AllNotes { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string theNote;
+        public string TheNote
+        {
+            //Makes theNote change in both the view model and in the code!
+            get => theNote;
+            set
+            {
+                theNote = value;
+
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command SaveCommand { get; }
+        public Command EraseCommand { get; }
+        
+       
+
+    }
+}
